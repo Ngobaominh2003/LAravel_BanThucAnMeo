d9a86fe101357d3ab5f2822cd9310f9ff8af6f62
Ngobaominh2003 <dongchieulienphuong.hy@gmail.com>
2024-05-24 15:56:07 +0700

    init project

--- buku/resources/views/Home/index.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- index.blade.php -->
-@extends('Home.Menu')
-
-@section('content')
-    <!-- Nội dung của trang index -->
-@endsection
--- buku/resources/views/Home/layout.blade.php
+++
@@ -1,1 +0,0 @@
-@include('Home.Menu', ['loaisanphams' => $loaisanphams])
